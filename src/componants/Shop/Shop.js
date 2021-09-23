7c7c932db72aa98754a19f18f1e9100fef64bc0d
rakibulfarhad <mraisfa@gmail.com>
2021-09-24 01:07:23 +0600

    cart update

--- src/componants/Shop/Shop.js
+++ src/componants/Shop/Shop.js
@@ -1,39 +1,78 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../cart/cart'
 import Product from '../Product/Product';
+import { addToDb, getStoredCart } from '../../utilities/fakedb'
 import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
+    const [displayProducts, setDisplayProducts] = useState([]);
 
     useEffect(() => {
         fetch('./Products.JSON')
             .then(res => res.json())
-            .then(data => setProducts(data))
+            .then(data => {
+                setProducts(data);
+                setDisplayProducts(data);
+            })
     }, [])
+
+    useEffect(() => {
+        if(products.length){
+            const savedCart = getStoredCart();
+            const storedCart = [];
+            for(const key in savedCart){
+                const addedProduct = products.find(product => product.key === key);
+                if(addedProduct){
+                    const quantity = savedCart[key];
+                    addedProduct.quantity = quantity;
+                    storedCart.push(addedProduct);
+                }
+                
+            }
+            setCart(storedCart);
+        }
+    }, [products])
 
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        // save to local storage (for now)
+        addToDb(product.key);
+    }
+
+    const handleSearch = event => {
+        const searchText = (event.target.value);
+        const matchProducts = products.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase()));
+        setDisplayProducts(matchProducts);
+        console.log(matchProducts.length);
     }
 
     return (
-        <div className="shop-container">
-            <div className="product-container">
-                <h3>Products:{products.length}</h3>
-                {
-                    products.map(product => <Product
-                        key={product.key}
-                        product={product}
-                        handleAddToCart={handleAddToCart}
-                    ></Product>)
-                }
+        <>
+            <div className="search-container">
+                <input 
+                type="text"
+                onChange={handleSearch} 
+                placeholder="Search-Product" />
             </div>
+            <div className="shop-container">
+                    <div className="product-container">
+                    <h3>Products:{products.length}</h3>
+                    {
+                        displayProducts.map(product => <Product
+                            key={product.key}
+                            product={product}
+                            handleAddToCart={handleAddToCart}
+                        ></Product>)
+                    }
+                </div>
             <div className="cart-container">
                 <Cart cart={cart}></Cart>
             </div>
-        </div>
+            </div>
+        </>
     );
 };
 
--- src/componants/cart/cart.js
+++ src/componants/cart/cart.js
@@ -3,16 +3,32 @@ import './cart.css';
 
 const cart = (props) => {
     const { cart } = props;
+   
+    // const totalReducer = (previous, product) => previous + product.price;
+    // const total = cart.reduce( totalReducer, 0 );
+    let totalQuantity = 0;
     let total = 0;
     for(const product of cart){
-        total = total = total + product.price;
+        if(!product.quantity){
+            product.quantity = 1;
+        }
+        total = total + product.price * product.quantity;
+        totalQuantity = totalQuantity + product.quantity;
     }
+
+    const shipping = total > 0 ? 15:0;
+    const tax = (total + shipping) * 0.10;
+    const grandTotal = total + shipping + tax;
+
     return (
         <div>
             <h3>Order Summary</h3>
-            <h5>Items Ordered: {props.cart.length}</h5>
+            <h5>Items Ordered: {totalQuantity}</h5>
             <br />
-            <p>Total: {total}</p>
+            <p>Total: {total.toFixed(2)}</p>
+            <p>Shipping: {shipping}</p>
+            <p>Tax: {tax.toFixed(2)}</p>
+            <p>Grand Total: {grandTotal.toFixed(2)}</p>
         </div>
     );
 };
